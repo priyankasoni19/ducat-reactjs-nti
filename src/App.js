f7d4a7def6502071923450bbe0e7d4b43f295749
priyanka soni <157217093+priyankasoni19@users.noreply.github.com>
2025-05-26 00:27:35 +0530

    ps

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import HeroSection from './components/sections/HeroSection';
+import WhyDucatSection from './components/sections/WhyDucatSection';
+import CoursesSection from './components/sections/CoursesSection';
+import PlacementSection from './components/sections/PlacementSection';
+import TestimonialsSection from './components/sections/TestimonialsSection';
+import RecruitersSection from './components/sections/RecruitersSection';
+
+function App() {
+  return (
+    <div>
+      <HeroSection />
+      <WhyDucatSection />
+      <CoursesSection />
+      <PlacementSection />
+      <TestimonialsSection />
+      <RecruitersSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sections/CoursesSection.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CoursesSection() {
+  return (
+    <div>CoursesSection</div>
+  )
+}
