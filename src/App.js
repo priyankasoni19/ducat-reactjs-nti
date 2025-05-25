8e71b310e1e8ad4b3cfe429a1c5c39a21f90703b
priyanka soni <157217093+priyankasoni19@users.noreply.github.com>
2025-05-26 01:17:39 +0530

    ps

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import CoursesSection from './components/sections/CoursesSection';
 import PlacementSection from './components/sections/PlacementSection';
 import TestimonialsSection from './components/sections/TestimonialsSection';
 import RecruitersSection from './components/sections/RecruitersSection';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <HeroSection />
       <WhyDucatSection />
       <CoursesSection />
       <PlacementSection />
       <TestimonialsSection />
       <RecruitersSection />
+       <Footer/>
     </div>
   );
 }
